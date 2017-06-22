0b846169aa5f597a13ccfb1aee93121f3e34f4f8
Ellpeck <megamaximal@gmail.com>
2017-06-22 15:32:04 +0200

    fix modding setup and add documentation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RockBottomModding
 A gradle modding setup with an example mod to be used for modding Rock Bottom
+
+## Setting up a development environment
+To set up an environment to make a mod, simply do the following:
+
+1. Import the gradle project into your IDE 
+    * For IntelliJ, simply use `File` `Open...` and select the `build.gradle` file to import the project
+    * For Eclipse, additionally run the gradle task `eclipse` to set up an Eclipse project
+    
+2. Run the gradle tasks `createIntelliJRuns` or `createEclipseRuns` for IntelliJ or Eclipse, respectively. This will create a run configuration that you can use to test your mod.
+    * For Eclipse, to run the project, right-click `Launcher.launch` to launch the project
+
+## Compiling the mod
+To make a jar containing your mod that you can then put into the `/mods` folder of your game instance, simple do the following: 
+
+2. Run the gradle task `gradlew build` to compile a mod jar
+1. You can then find the jar in `/build/libs`
--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,41 +1,48 @@
 package com.example.examplemod;
 
-import de.ellpeck.rockbottom.ContentRegistry;
-import de.ellpeck.rockbottom.api.*;
-import de.ellpeck.rockbottom.construction.BasicRecipe;
-
+import de.ellpeck.rockbottom.api.GameContent;
+import de.ellpeck.rockbottom.api.IApiHandler;
+import de.ellpeck.rockbottom.api.IGameInstance;
+import de.ellpeck.rockbottom.api.RockBottomAPI;
+import de.ellpeck.rockbottom.api.assets.IAssetManager;
+import de.ellpeck.rockbottom.api.construction.BasicRecipe;
+import de.ellpeck.rockbottom.api.event.IEventHandler;
+import de.ellpeck.rockbottom.api.item.ItemInstance;
+import de.ellpeck.rockbottom.api.mod.IMod;
 import org.newdawn.slick.util.Log;
 
-public class ExampleMod implements IMod {
+public class ExampleMod implements IMod{
 
     @Override
-    public String getDisplayName() {
+    public String getDisplayName(){
         return "ExampleMod";
     }
 
     @Override
-    public String getId() {
+    public String getId(){
         return "examplemod";
     }
 
     @Override
-    public String getVersion() {
+    public String getVersion(){
         return "1.0.0";
     }
 
     @Override
-    public String getResourceLocation() {
+    public String getResourceLocation(){
         return "/assets/examplemod";
     }
 
     @Override
-    public String getDescription() {
+    public String getDescription(){
         return "This is the example mod for RockBottom";
     }
 
     @Override
-    public void preInit(IGameInstance game, IApiHandler apiHandler, IEventHandler eventHandler) {
+    public void init(IGameInstance game, IAssetManager assetManager, IApiHandler apiHandler, IEventHandler eventHandler){
         Log.info("Starting ExampleMod for RockBottom");
-        RockBottomAPI.MANUAL_RECIPES.add(new BasicRecipe(new ItemInstance(ContentRegistry.ITEM_SUPER_TOOL), new ItemInstance(ContentRegistry.TILE_DIRT, 64)));
+
+        RockBottomAPI.MANUAL_CONSTRUCTION_RECIPES.add(new BasicRecipe(new ItemInstance(GameContent.ITEM_SUPER_TOOL), new ItemInstance(GameContent.TILE_DIRT, 64)));
+        Log.info("Localized text: "+assetManager.localize(RockBottomAPI.createRes(this, "test")));
     }
 }
